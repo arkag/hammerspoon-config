99a8414a5478cd6a0f856892a98c4e4b4bf737f1
Alexander Kagno <arkag@orrorin.lan>
2017-11-12 20:26:21 -0500

    automated commit

--- init.lua
+++ init.lua
@@ -34,13 +34,13 @@ local function paste()
   if not url and not paste then
     pp = hs.task.new("/usr/local/bin/pngpaste", nil, {home .. "/.paste.png"})
     pp:start()
-    fn = hs.task.new("/usr/local/bin/fb", function(exitCode, stdOut, stdErr) hs.pasteboard.setContents(stdOut) end, {home.."/.paste.png"}) hs.alert.show("Image uploaded")
+    fn = hs.task.new("/usr/local/bin/fb", function(exitCode, stdOut, stdErr) hs.pasteboard.setContents(stdOut) end, home.."/.paste.png") hs.alert.show("Image uploaded")
   elseif url then
-    fn = hs.task.new("/usr/local/bin/fb", function(exitCode, stdOut, stdErr) hs.pasteboard.setContents(stdOut) end, {url})
+    fn = hs.task.new("/usr/local/bin/fb", function(exitCode, stdOut, stdErr) hs.pasteboard.setContents(stdOut) end, url)
     fn:setInput(url)
     hs.alert.show("URL shortened")
   else
-    fn = hs.task.new("/usr/local/bin/fb", function(exitCode, stdOut, stdErr) hs.pasteboard.setContents(stdOut) end, {paste})
+    fn = hs.task.new("/usr/local/bin/fb", function(exitCode, stdOut, stdErr) hs.pasteboard.setContents(stdOut) end, paste)
     hs.alert.show("Text pasted")
   end
   fn:start()
